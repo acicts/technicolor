64ecb5d1fad60ad9f1d6aea05f70cdd3193071ac
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2022-07-14 22:52:15 +0530

    added regex validation and refractor

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,7 @@
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 
-const FormComponent = ({ name, label, optional, register }) => {
+const FormComponent = ({ name, label, optional, pattern }) => {
+  const { register } = useFormContext();
   return (
     <div className="flex flex-col my-5">
       <label className="text-xl font-[Poppins] text-gray-700 shadow-md">
@@ -9,7 +10,7 @@ const FormComponent = ({ name, label, optional, register }) => {
       </label>
 
       <input
-        {...register(name, { required: !optional })}
+        {...register(name, { required: !optional, pattern: pattern })}
         className="rounded-md border-gray-400 shadow-md h-10 border px-3 w-56 lg:w-auto outline-none focus:bg-gray-100"
       />
     </div>
@@ -17,47 +18,66 @@ const FormComponent = ({ name, label, optional, register }) => {
 };
 
 const Register = () => {
-  const { register, handleSubmit } = useForm();
+  const { handleSubmit, errors, ...methods } = useForm();
   return (
-    <form
-      action=""
-      onSubmit={handleSubmit((data) => {
-        let form = document.getElementById("registration-form");
-        form.submit(data);
-      })}
-      method="post"
-      id="register"
-      className="mt-40 lg:w-[50rem] mx-auto "
-    >
-      <span className="section-title">Register Now</span>
-      <div className="w-[95%] lg:w-full h-[45rem] lg:h-[50rem]  shadow-lg border-4 border-black px-10 lg:px-40 pt-5 form rounded-md">
-        <FormComponent name="name" label="Full Name" register={register} />
-        <FormComponent name="class" label="Class" register={register} />
-        <FormComponent
-          name="admission"
-          label="Admission Number"
-          register={register}
-        />
-        <FormComponent name="phone" label="Phone Number" register={register} />
-        <FormComponent
-          name="whatsapp"
-          label="Whatsapp Number"
-          register={register}
-        />
-        <FormComponent
-          name="discord"
-          label="Discord Username"
-          optional
-          register={register}
-        />
-        <input
-          type="submit"
-          value="Register"
-          className="btn w-48 lg:w-80 h-16  bg-white lg:mt-10 outline-none"
-        />
-      </div>
-    </form>
+    <FormProvider {...{ handleSubmit, errors, ...methods }}>
+      <form
+        action=""
+        onSubmit={handleSubmit((data) => {
+          let form = document.getElementById("register");
+          form.submit(data);
+        })}
+        method="post"
+        id="register"
+        className="mt-40 lg:w-[50rem] mx-auto "
+      >
+        <span className="section-title">Register Now</span>
+        <div className="w-[95%] lg:w-full h-[50rem] lg:h-[55rem]  shadow-lg border-4 border-black px-10 lg:px-40 pt-5 form rounded-md">
+          <FormComponent name="name" label="Full Name" />
+          <FormComponent
+            name="class"
+            label="Class"
+            pattern={/([6-9]|1[0-3])-(\d|1[01])/g}
+          />
+          <FormComponent
+            name="email"
+            label="Email"
+            pattern={/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/g}
+          />
+          <FormComponent
+            name="admission"
+            label="Admission Number"
+            pattern={/\d{5}/g}
+          />
+          <FormComponent
+            type="tel"
+            name="phone"
+            label="Phone Number"
+            pattern={/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/g}
+          />
+          <FormComponent
+            type="tell"
+            name="whatsapp"
+            label="Whatsapp Number"
+            pattern={/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/g}
+          />
+          <FormComponent
+            name="discord"
+            label="Discord Username"
+            pattern={/.+#\d{4}/g}
+            optional
+          />
+          <input
+            type="submit"
+            value="Register"
+            className="btn w-48 lg:w-80 h-16  bg-white lg:mt-10 outline-none"
+          />
+        </div>
+      </form>
+    </FormProvider>
   );
 };
 
 export default Register;
+
+// Name, Class, Admission, Email, Phone, Whatsapp, Discord
