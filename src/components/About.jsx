0ec7c0dc99b6bcdec4e750bcbd50768695e16f56
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2022-07-16 05:06:26 +0530

    changed fonts of About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,22 +2,31 @@ import Computer from "../assets/computer.png";
 
 const About = () => (
   <div
-    className="flex lg:flex-row py-10 lg:py-20 justify-around flex-col-reverse  sm:h-full"
+    className="flex lg:flex-row py-10 lg:py-20 justify-around flex-col-reverse  "
     id="about"
   >
-    <div className="pl-2 flex flex-col w-full lg:w-[45%]">
-      <span className="text-[300%] lg:text-[5rem] font-bold font-[Montserrat] leading-[4rem] lg:leading-[5rem] mb-10">
-        Intraschool <span className="font-[NeonLedLight]">Graphic Design</span>{" "}
+    <div className="pl-2 flex flex-col w-full lg:w-[50%]">
+      <span className="text-[2.8rem] md:text-[4rem] lg:text-[4.5rem]  font-[Praktika-Bold] leading-[4rem] lg:leading-[5rem] mb-10 mx-auto lg:mx-0 text-center lg:text-left">
+        Intraschool
+        <br />
+        <span className="font-[Praktika-ExtraBold]">Graphic Design</span>
+        <br />
         Competition
       </span>
-      <span className="font-italic italic">
+      <span className="italic mx-auto lg:mx-0 font-[Poppins]">
         "The way to get started is to quit talking and begin doing."
       </span>
-      <span className="font-bold">- Walt Disney</span>
+      <span className="font-bold mx-auto lg:mx-0 font-[Montserrat]">
+        - Walt Disney
+      </span>
       <button className="btn mx-auto lg:mx-0 py-4 mt-20 w-64">Register</button>
     </div>
     <div className="lg:w-[45%] sm:w-full sm:mt-20 lg:m-0">
-      <img className="m-auto w-[82%]" src={Computer} alt="a computer" />
+      <img
+        className="m-auto w-[85%] max-w-lg"
+        src={Computer}
+        alt="a computer"
+      />
     </div>
   </div>
 );
