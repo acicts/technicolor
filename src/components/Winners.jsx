8d9786201ddd6ea0ba498808850816dcc4a5d463
chamindujs <chamindujs@gmail.com>
2022-09-10 19:14:17 +0530

    Winners

---
+++ src/components/Winners.jsx
@@ -0,0 +1,80 @@
+const Winners = () => (
+    <div className="mt-20 w-3/4 min-w-[320px] mx-auto">
+      <span className="section-title" id="winners">
+        Winners
+      </span>
+      <span className="section-subtitle" id="winners">
+        Poster Design
+      </span>
+      <div className="flex flex-wrap justify-evenly gap-5 gap-x-10">
+        <div
+          className="contact  flex justify-center items-center py-100 flex-col"
+        >
+          Dinindu Gamage
+          <p className="text-[20px]">Junior Category</p>
+        </div>
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Nethula Peiris
+          <p className="text-[20px]">Intermediate Category</p>
+        </div>
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Riviru Methsara
+          <p className="text-[20px]">Senior Category</p>
+        </div>
+      </div>
+
+      <span className="section-subtitle mt-20" id="winners">
+      3D Design
+      </span>
+      <div className="flex flex-wrap justify-evenly gap-5 gap-x-10">
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Chanidu Nimnadh
+          <p className="text-[20px]">Junior Category</p>
+        </div>
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Hivin Kenula
+          <p className="text-[20px]">Intermediate Category</p>
+        </div>
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Dilruksh Wijesekara
+          <p className="text-[20px]">Senior Category</p>
+        </div>
+      </div>
+
+      <span className="section-subtitle mt-20" id="winners">
+      Video Editing
+      </span>
+      <div className="flex flex-wrap justify-evenly gap-5 gap-x-10">
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Risul Mindiya
+          <p className="text-[20px]">Junior Category</p>
+        </div>
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Sathija Nimthara
+          <p className="text-[20px]">Intermediate Category</p>
+        </div>
+        <div
+          className="contact flex justify-center items-center py-100 flex-col"
+        >
+          Nivodh Arampath
+          <p className="text-[20px]">Senior Category</p>
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default Winners;
